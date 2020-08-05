b808fff62f6a0f2e09fe87da846d12d099aff4cf
Jefford Mamacus <jefford@qwigo.com>
2020-08-05 23:46:51 +0800

    Fixed api_url string

--- bunnyhop/stats.py
+++ bunnyhop/stats.py
@@ -4,8 +4,7 @@ from bunnyhop.base import BaseBunny
 class Stats(BaseBunny):
     
     def get(self, dateFrom, dateTo, pullZone, serverZoneId):
-        api_url = self._URL + '/statistics?dateFrom=/' + dateFrom + '&dateTo='
-        + dateTo + '&pullZone=' + pullZone + '&serverZoneId=' + serverZoneId
+        api_url = self._URL + '/statistics?dateFrom=/' + dateFrom + '&dateTo=' + dateTo + '&pullZone=' + pullZone + '&serverZoneId=' + serverZoneId
         header = self._GetHeaders()
         response = self._CallApi(api_url, "GET", header)
         return self._FormatResponse(response)
