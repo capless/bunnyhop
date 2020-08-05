63d78582138c5b03bdd9b83f2422cecc4ab3d7d3
Jefford Mamacus <jefford@qwigo.com>
2020-08-06 00:54:16 +0800

    Added feature classes

--- bunnyhop/core.py
+++ bunnyhop/core.py
@@ -1,12 +1,16 @@
 from bunnyhop.storage import Storage
 from bunnyhop.zone import Zone
+from bunnyhop.stats import Stats
+from bunnyhop.billing import Billing
+from bunnyhop.purge import Purge
+from bunnyhop.storagezone import StorageZone
 
 class Bunny(object):
 
     def __init__(self, api_key):
         self.Zone = Zone(api_key)
-        # self.Purge = Purge(api_key)
+        self.Purge = Purge(api_key)
         self.Storage = Storage(api_key)
-        # self.StorageZone = StorageZone(api_key)
-        # self.Stats = Stats(api_key)
-        # self.Billing = Billing(api_key)
+        self.StorageZone = StorageZone(api_key)
+        self.Stats = Stats(api_key)
+        self.Billing = Billing(api_key)
