fd23eaf21462520fe217375a50cf685c81d2c132
Jefford Mamacus <jefford@qwigo.com>
2020-08-05 23:27:26 +0800

    Added storage

--- bunnyhop/storage.py
+++ bunnyhop/storage.py
@@ -2,4 +2,32 @@ from bunnyhop.base import BaseBunny
 
 
 class Storage(BaseBunny):
-    pass
+
+    def create(self, name, main_storage_region, replica_regions):
+        api_url = self._URL + '/storagezone'
+        header = self._GetHeaders()
+        api_data = {
+            'Name': name,
+            'Region': main_storage_region,
+            'ReplicationRegions': replica_regions,
+        }
+        response = self._CallApi(api_url, "POST", header, api_data)
+        return self._FormatResponse(response)
+
+    def all(self):
+        api_url = self._URL + '/storagezone'
+        header = self._GetHeaders()
+        response = self._CallApi(api_url, "GET", header)
+        return self._FormatResponse(response)
+
+    def delete(self, id):
+        api_url = self._URL + '/storagezone/' + id
+        header = self._GetHeaders()
+        response = self._CallApi(api_url, "DELETE", header)
+        return self._FormatResponse(response)
+
+    def get(self, id):
+        api_url = self._URL + '/storagezone/' + id
+        header = self._GetHeaders()
+        response = self._CallApi(api_url, "GET", header)
+        return self._FormatResponse(response)
