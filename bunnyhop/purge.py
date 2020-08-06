f19001d7af03486e0c920a6cee837d76d48bdb8c
Jefford Mamacus <jefford@qwigo.com>
2020-08-06 16:24:13 +0800

    fixed code structure

--- bunnyhop/purge.py
+++ bunnyhop/purge.py
@@ -4,7 +4,5 @@ from bunnyhop.base import BaseBunny
 class Purge(BaseBunny):
     
     def create(self, url):
-        api_url = self.endpoint_url + '/purge?url=' + url
-        header = self.get_header()
-        response = self.call_api(api_url, "POST", header)
-        return self.format_response(response)
+        return self.call_api(f"{self.endpoint_url}/purge?url=", "POST", self.get_header())
+        
