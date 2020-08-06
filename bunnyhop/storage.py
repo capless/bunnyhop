2aa9a2d44dc952e1dccec0db9232301a5ab017f0
Jefford Mamacus <jefford@qwigo.com>
2020-08-06 16:40:19 +0800

    fixed code structure

--- bunnyhop/storage.py
+++ bunnyhop/storage.py
@@ -4,30 +4,20 @@ from bunnyhop.base import BaseBunny
 class Storage(BaseBunny):
 
     def create(self, name, main_storage_region, replica_regions):
-        api_url = self.endpoint_url + '/storagezone'
-        header = self.get_header()
         api_data = {
             'Name': name,
             'Region': main_storage_region,
             'ReplicationRegions': replica_regions,
         }
-        response = self.call_api(api_url, "POST", header, api_data)
-        return self.format_response(response)
+        return self.call_api(f"{self.endpoint_url}/storagezone/", "POST", self.get_header(), api_data)
+        
 
     def all(self):
-        api_url = self.endpoint_url + '/storagezone'
-        header = self.get_header()
-        response = self.call_api(api_url, "GET", header)
-        return response.text
+        return self.call_api(f"{self.endpoint_url}/storagezone", "GET", self.get_header())
 
     def delete(self, id):
-        api_url = self.endpoint_url + '/storagezone/' + id
-        header = self.get_header()
-        response = self.call_api(api_url, "DELETE", header)
-        return self.format_response(response)
+        return self.call_api(f"{self.endpoint_url}/storagezone/{id}", "DELETE", self.get_header())
+    
 
     def get(self, id):
-        api_url = self.endpoint_url + '/storagezone/' + id
-        header = self.get_header()
-        response = self.call_api(api_url, "GET", header)
-        return response.text
+        return self.call_api(f"{self.endpoint_url}/storagezone/{id}", "GET", self.get_header())
