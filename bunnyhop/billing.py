014dbba9d6572730930ab2776b8262e76e61f395
Jefford Mamacus <jefford@qwigo.com>
2020-08-06 16:18:40 +0800

    fixed code structure

--- bunnyhop/billing.py
+++ bunnyhop/billing.py
@@ -4,13 +4,9 @@ from bunnyhop.base import BaseBunny
 class Billing(BaseBunny):
     
     def get(self):
-        api_url = self.endpoint_url + '/billing'
-        header = self.get_header()
-        response = self.call_api(api_url, "GET", header)
-        return self.format_response(response)
+        return self.call_api(f"{self.endpoint_url}/billing", "GET", self.get_header())
 
     def applycode(self, couponCode):
-        api_url = self.endpoint_url + '/billing/applycode?couponCode=' + couponCode
+        api_url = f"{self.endpoint_url}/billing/applycode?couponCode={couponCode}"
         header = self.get_header()
-        response = self.call_api(api_url, "GET", header)
-        return self.format_response(response)
+        return self.call_api(api_url, "GET", header)
