414c4739fdad47bf09dace313bf99d8d9b6a2c96
Jefford Mamacus <jefford@qwigo.com>
2020-08-06 16:32:34 +0800

    fixed code structure

--- bunnyhop/stats.py
+++ bunnyhop/stats.py
@@ -4,7 +4,6 @@ from bunnyhop.base import BaseBunny
 class Stats(BaseBunny):
     
     def get(self, dateFrom, dateTo, pullZone, serverZoneId):
-        api_url = self.endpoint_url + '/statistics?dateFrom=/' + dateFrom + '&dateTo=' + dateTo + '&pullZone=' + pullZone + '&serverZoneId=' + serverZoneId
+        api_url = f"{self.endpoint_url}/statistics?dateFrom=/{dateFrom}&dateTo={dateTo}&pullZone={pullZone}&serverZoneId={serverZoneId}"
         header = self.get_header()
-        response = self.call_api(api_url, "GET", header)
-        return self.format_response(response)
+        return self.call_api(api_url, "GET", header)
