cb884b501b7e7f6491f7f97d997fd3f215ed2cbf
Jefford Mamacus <jefford@qwigo.com>
2020-08-06 17:48:48 +0800

    Added storage all

--- bunnyhop/storagezone.py
+++ bunnyhop/storagezone.py
@@ -1,10 +1,40 @@
 from bunnyhop.base import BaseBunny
-
+import json
 
 class StorageZone(BaseBunny):
     
     def get(self, id):
-        api_url = self.endpoint_url + '/storagezone/' + id
-        header = self.get_header()
-        response = self.call_api(api_url, "GET", header)
-        return response.text
+        response = self.call_api(f"{self.endpoint_url}/storagezone/{id}", "GET", self.get_header(), format=False)
+        response_json = json.loads(response.text)
+        return StorageObject(response_json['Name'])
+    
+
+class StorageObject(BaseBunny):
+    
+    def __init__(self, zone_name):
+        self.zone_name = zone_name
+        self.endpoint_url = "https://private-anon-3bc35a36a9-bunnycdnstorage.apiary-mock.com/"
+    
+    def get_header(self):
+        header = {
+            'Content-Type': 'application/json',
+        }
+        return header
+    
+    def all(self, path):
+        return self.call_api(f"{self.endpoint_url}/{self.zone_name}/{path}", "GET", self.get_header())
+    
+    def get(self):
+        pass
+    
+    def download(self):
+        pass
+    
+    def delete(self):
+        pass
+    
+    def upload_file(self):
+        pass
+
+    def create_json(self):
+        pass
