af26e614127f4fa7e0e6561b22f41f178348ee06
Jefford Mamacus <jefford@qwigo.com>
2020-08-07 19:49:53 +0800

    Fixed code indentations

--- tests.py
+++ tests.py
@@ -35,7 +35,12 @@ class TestStats(unittest.TestCase):
         self.b = None
 
     def test_get(self):
-        response_json = json.loads(self.b.Stats.get(dateFrom='2018-12-01', dateTo='2020-01-01', pullZone='example-zone', serverZoneId='serverZoneID'))
+        response_json = json.loads(self.b.Stats.get(
+            dateFrom='2018-12-01',
+            dateTo='2020-01-01',
+            pullZone='example-zone',
+            serverZoneId='serverZoneID'
+        ))
         self.assertEqual(response_json['status_code'], 200)
 
 
@@ -50,7 +55,8 @@ class TestPurge(unittest.TestCase):
         self.b = None
 
     def test_create(self):
-        response_json = json.loads(self.b.Purge.create(url='https://myzone.b-cdn.net/style.css'))
+        response_json = json.loads(self.b.Purge.create(
+            url='https://myzone.b-cdn.net/style.css'))
         self.assertEqual(response_json['status_code'], 200)
 
 
@@ -80,21 +86,21 @@ class TestStorageObject(unittest.TestCase):
     def tearDownClass(self):
         self.b = None
         self.zone = None
-        
+
     def test_all(self):
         response_json = json.loads(self.zone.all('path'))
         self.assertEqual(response_json['status_code'], 200)
 
     def test_get(self):
-        response_json = json.loads(self.zone.get('path','filename'))
+        response_json = json.loads(self.zone.get('path', 'filename'))
         self.assertEqual(response_json['status_code'], 200)
 
     def test_delete(self):
-        response_json = json.loads(self.zone.delete('path','filename'))
+        response_json = json.loads(self.zone.delete('path', 'filename'))
         self.assertEqual(response_json['status_code'], 200)
 
     def test_upload_file(self):
-        response_json = json.loads(self.zone.delete('dest_path','local_path'))
+        response_json = json.loads(self.zone.delete('dest_path', 'local_path'))
         self.assertEqual(response_json['status_code'], 200)
 
     def test_create_file(self):
@@ -109,7 +115,7 @@ class TestStorageObject(unittest.TestCase):
     def test_create_json(self):
         open_mock = mock_open()
         json_name = '23.json'
-        json_content = {'first_name':'Michael', 'last_name': 'Jordan'}
+        json_content = {'first_name': 'Michael', 'last_name': 'Jordan'}
         with patch("bunnyhop.storage_zone.open", open_mock, create=True):
             self.zone.create_json(json_name, json_content)
         open_mock.assert_called_with(json_name, "w+")
@@ -129,7 +135,8 @@ class TestStorage(unittest.TestCase):
     def test_create(self):
         response_json = json.loads(self.b.Storage.create())
         self.assertEqual(response_json['status_code'], 201)
-        response_json = json.loads(self.b.Storage.create(name='example-a', main_storage_region='NY', replica_regions=['DE', 'SG', 'SYD']))
+        response_json = json.loads(self.b.Storage.create(
+            name='example-a', main_storage_region='NY', replica_regions=['DE', 'SG', 'SYD']))
         self.assertEqual(response_json['status_code'], 201)
 
     def test_all(self):
@@ -167,7 +174,7 @@ class TestZone(unittest.TestCase):
             Type=0,
             OriginUrl='https://example.org',
             StorageZoneId='storage-zone-id'
-            ))
+        ))
         self.assertEqual(response_json['status_code'], 201)
 
     def test_list(self):
@@ -178,7 +185,7 @@ class TestZone(unittest.TestCase):
         response_json = json.loads(self.b.Zone.update(id=1))
         self.assertEqual(response_json['status_code'], 200)
         response_json = json.loads(self.b.Zone.update(
-            id='zone-id', #Positional Argument
+            id='zone-id',
             OriginUrl='http://example.org',
             AllowedReferrers=['app.example.org', 'www.example.org'],
             BlockedIps=['122.33.22.11'],
@@ -211,7 +218,7 @@ class TestZone(unittest.TestCase):
             AWSSigningSecret='SK_EXAMPLESECRETKET',
             EnableTLS1=True,
             EnableTLS1_1=True
-            ))
+        ))
         self.assertEqual(response_json['status_code'], 200)
 
     def test_delete(self):
@@ -220,7 +227,7 @@ class TestZone(unittest.TestCase):
 
     def test_purge(self):
         response_json = json.loads(self.b.Zone.purge(id=1))
-        self.assertEqual(response_json['status_code'], 200) 
+        self.assertEqual(response_json['status_code'], 200)
 
     def test_create_edge_rule(self):
         response_json = json.loads(self.b.Zone.create_edge_rule(id=1))
@@ -228,15 +235,16 @@ class TestZone(unittest.TestCase):
         response_json = json.loads(self.b.Zone.create_edge_rule(
             id='myzone',
             Guid="6a2e94df-8aa9-4cd2-b89d-16752102ef9f",
-            ActionParameter1 = "My-Header",
-            ActionParameter2 = "HeaderValue",
-            Enabled = True,
-            Description = "My header value",
-            ActionType = 0,
-            TriggerMatchingType = 1,
-            Triggers = []
-            ))
+            ActionParameter1="My-Header",
+            ActionParameter2="HeaderValue",
+            Enabled=True,
+            Description="My header value",
+            ActionType=0,
+            TriggerMatchingType=1,
+            Triggers=[]
+        ))
         self.assertEqual(response_json['status_code'], 201)
 
-if __name__ == '__main__': 
+
+if __name__ == '__main__':
     unittest.main()
