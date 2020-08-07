12462d0aed6d73e5ff53137cf9f14accf7e4d1b2
Jefford Mamacus <jefford@qwigo.com>
2020-08-07 19:28:34 +0800

    Added TestZone

--- tests.py
+++ tests.py
@@ -145,5 +145,98 @@ class TestStorage(unittest.TestCase):
         self.assertEqual(response_json['status_code'], 200)
 
 
+class TestZone(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.b = Bunny('api-key')
+
+    @classmethod
+    def tearDownClass(self):
+        self.b = None
+
+    def test_get(self):
+        response_json = json.loads(self.b.Zone.get('myzone'))
+        self.assertEqual(response_json['status_code'], 200)
+
+    def test_create(self):
+        response_json = json.loads(self.b.Zone.create())
+        self.assertEqual(response_json['status_code'], 201)
+        response_json = json.loads(self.b.Zone.create(
+            Name='myzone',
+            Type=0,
+            OriginUrl='https://example.org',
+            StorageZoneId='storage-zone-id'
+            ))
+        self.assertEqual(response_json['status_code'], 201)
+
+    def test_list(self):
+        response_json = json.loads(self.b.Zone.list())
+        self.assertEqual(response_json['status_code'], 200)
+
+    def test_update(self):
+        response_json = json.loads(self.b.Zone.update(id=1))
+        self.assertEqual(response_json['status_code'], 200)
+        response_json = json.loads(self.b.Zone.update(
+            id='zone-id', #Positional Argument
+            OriginUrl='http://example.org',
+            AllowedReferrers=['app.example.org', 'www.example.org'],
+            BlockedIps=['122.33.22.11'],
+            EnableCacheSlice=False,
+            EnableGeoZoneUS=True,
+            EnableGeoZoneEU=True,
+            EnableGeoZoneASIA=True,
+            EnableGeoZoneSA=True,
+            EnableGeoZoneAF=True,
+            ZoneSecurityEnabled=True,
+            ZoneSecurityIncludeHashRemoteIP=True,
+            IgnoreQueryStrings=True,
+            MonthlyBandwidthLimit=1073741824,
+            AccessControlOriginHeaderExtensions=['jpg', 'png'],
+            EnableAccessControlOriginHeader=True,
+            BlockRootPathAccess=True,
+            EnableWebpVary=True,
+            EnableLogging=True,
+            DisableCookies=False,
+            BudgetRedirectedCountries=['RU', 'BR'],
+            BlockedCountries=['RU', 'BR'],
+            EnableOriginShield=True,
+            OriginShieldZoneCode='FR',
+            AddCanonicalHeader=0,
+            CacheControlMaxAgeOverride=-1,
+            AddHostHeader=True,
+            AWSSigningEnabled=True,
+            AWSSigningKey='AK_EXAMPLEKEY',
+            AWSSigningRegionName='us-east-1',
+            AWSSigningSecret='SK_EXAMPLESECRETKET',
+            EnableTLS1=True,
+            EnableTLS1_1=True
+            ))
+        self.assertEqual(response_json['status_code'], 200)
+
+    def test_delete(self):
+        response_json = json.loads(self.b.Zone.delete(id=1))
+        self.assertEqual(response_json['status_code'], 204)
+
+    def test_purge(self):
+        response_json = json.loads(self.b.Zone.purge(id=1))
+        self.assertEqual(response_json['status_code'], 200) 
+
+    def test_create_edge_rule(self):
+        response_json = json.loads(self.b.Zone.create_edge_rule(id=1))
+        self.assertEqual(response_json['status_code'], 201)
+        response_json = json.loads(self.b.Zone.create_edge_rule(
+            id='myzone',
+            Guid="6a2e94df-8aa9-4cd2-b89d-16752102ef9f",
+            ActionParameter1 = "My-Header",
+            ActionParameter2 = "HeaderValue",
+            Enabled = True,
+            Description = "My header value",
+            ActionType = 0,
+            TriggerMatchingType = 1,
+            Triggers = []
+            ))
+        self.assertEqual(response_json['status_code'], 201)
+
 if __name__ == '__main__': 
     unittest.main()
